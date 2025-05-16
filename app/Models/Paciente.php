a0a1567f24d954617d8a0ddd4b73313bf677ee63
angel-prz <191927151+angel-prz@users.noreply.github.com>
2025-05-15 21:01:04 -0300

    refactor: paciente

--- app/Models/Paciente.php
+++ app/Models/Paciente.php
@@ -5,7 +5,31 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Model;
 use App\Models\User;
 
-class Paciente extends User
+class Paciente extends Model
 {
-    //
+    protected $fillable = [
+        'tipo_paciente',
+    ];
+
+
+    //RELAÇÔES TERMINAR DEPOIS
+    //EXTENDS USER
+   /*  public function user()
+    {
+        return $this->belongsTo(User::class);
+    } */
+
+    //RELAÇÔES 1 PARA MUITOS (1toN)
+    /* public function consultas()
+    {
+        return $this->hasMany(Consulta::class);
+    } */
+
+    /* public function dispenca_ef()
+    {
+        return $this->hasOne(DispencaEf::class);
+    } */
+
+    
+
 }   
