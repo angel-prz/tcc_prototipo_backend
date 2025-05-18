7e411f9d18c409f46b9bc6cb4688b1e0e635bace
angel-prz <191927151+angel-prz@users.noreply.github.com>
2025-05-18 17:15:53 -0300

    feat: consulta model

--- app/Models/Consulta.php
+++ app/Models/Consulta.php
@@ -2,9 +2,29 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Testing\Fluent\Concerns\Has;
 
 class Consulta extends Model
 {
-    //
+    use HasFactory;
+    
+    protected $fillable = [
+        'id_paciente',
+        'id_profissional',
+        'status',
+        'observacao',
+        'data_hora',
+        // add any other fields that should be fillable
+    ];
+
+    public function paciente()
+    {
+        return $this->belongsTo(User::class, 'id_paciente');
+    }
+    public function profissional()
+    {
+        return $this->belongsTo(User::class, 'id_profissional');
+    }
 }
