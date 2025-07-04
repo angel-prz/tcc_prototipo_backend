e875ea11a05836983b5feeb03b3c76e99c41ed83
angel-prz <191927151+angel-prz@users.noreply.github.com>
2025-07-04 09:48:22 -0300

    Feat: Migration, Factory e Seeder de Consulta

--- database/factories/ConsultaFactory.php
+++ database/factories/ConsultaFactory.php
@@ -17,8 +17,8 @@ class ConsultaFactory extends Factory
     public function definition(): array
     {
         return [
-            'id_profissional' => User::factory(), // Ou use um ID válido
-            'id_paciente' => User::factory(),
+            'profissional_id' => User::factory(), 
+            'paciente_id' => User::factory(),
             'status' => $this->faker->randomElement(['agendada', 'realizada', 'cancelada']),
             'observacao' => $this->faker->optional()->sentence(),
             'data_hora' => $this->faker->dateTimeBetween('now', '+1 month'),
--- database/seeders/ConsultaSeeder.php
+++ database/seeders/ConsultaSeeder.php
@@ -4,6 +4,8 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
 use App\Models\Consulta;
+use App\Models\Profissional;
+use App\Models\Paciente;
 use App\Models\User;
 
 class ConsultaSeeder extends Seeder
@@ -13,16 +15,17 @@ class ConsultaSeeder extends Seeder
      */
     public function run(): void
     {
-        //
-        $profissionais = User::factory()->count(5)->create();
-        $pacientes = User::factory()->count(10)->create();
+        //consultas
+        $profissionais = Profissional::factory()->count(12)->create();
+        $pacientes = Paciente::factory()->count(100)->create();
 
         // Cria 20 consultas aleatórias
-        foreach (range(1, 20) as $i) {
+        foreach (range(1, 30) as $i) {
             Consulta::factory()->create([
-                'id_profissional' => $profissionais->random()->id,
-                'id_paciente' => $pacientes->random()->id,
+                'profissional_id' => $profissionais->random()->id,
+                'paciente_id' => $pacientes->random()->id,
             ]);
         }
     }
 }
+
