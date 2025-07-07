cabb665b106665ef32c19f9e545e8d6a108ae40b
Angel Pérez <191927151+angel-prz@users.noreply.github.com>
2025-07-07 19:49:23 +0000

    feat: controllers para a API Rest do tipo Recurso(resource) de API.

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -2,9 +2,15 @@
 
 namespace App\Http\Controllers\Api;
 
-use App\Http\Controllers\Controller;
+use App\Http\Controllers\Api\Controller;
+use App\Http\Requests\UserStoreRequest;
+use App\Http\Requests\UserUpdateRequest;
+use App\Http\Resources\UserCollectionResource;
+use App\Http\Resources\UserResource;
+use App\Http\Resources\UserStoredResource;
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Validation\ValidationException;
 
 class UserController extends Controller
 {
@@ -13,38 +19,54 @@ class UserController extends Controller
      */
     public function index()
     {
-        //
+        return new UserCollectionResource(User::all());
     }
 
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(UserStoreRequest $request)
     {
-        //
+        try {
+            return new UserStoredResource(User::create($request->validated()));
+        } catch (\Exception $e) {
+            return $this->errorHandler('Erro ao criar Usuario',$e);
+        }
     }
 
     /**
      * Display the specified resource.
      */
-    public function show(User $user)
+    public function show(User $User)
     {
-        //
+        return new UserResource($User);
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, User $user)
+    public function update(UserUpdateRequest $request, User $User)
     {
-        //
+        try {
+            $User->update($request->validated());
+            return new UserResource($User);
+        } catch (\Exception $e) {
+            return $this->errorHandler('Erro ao atualizar Usuario',$e);
+        }
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(User $user)
+    public function destroy(User $User)
     {
-        //
+        try {
+            $User->delete();
+            return (new UserResource($User))->additional([
+                    'message' => 'Usuario deletado com sucesso!'
+                ]);
+        } catch (\Exception $e) {
+            return $this->errorHandler('Erro ao atualizar Usuario',$e);
+        }
     }
 }
