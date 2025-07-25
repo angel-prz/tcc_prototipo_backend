83b86295012ebf7620a0ae52ac6418218cf3b5a1
angel-prz <191927151+angel-prz@users.noreply.github.com>
2025-07-25 10:58:51 -0300

    refactor: troca de token para teste

--- test/api/curl/user_index_c_token.sh
+++ test/api/curl/user_index_c_token.sh
@@ -5,4 +5,4 @@ url="http://127.0.0.1:8000/api/v1/users"
 
 # Send GET request using curl
 curl -X GET "$url" \
- -H  "Authorization: Bearer 2|Emz4wjYr1hkemHCEkDFgHmOD0V8yTG0H6Kz5rkle41c61f49"
+ -H  "Authorization: Bearer 1|S2G8HP8dx6SmVOlwWItg9L9YxypfQOJ2iQEFzXFX87386e53"
