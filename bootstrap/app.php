e6c3df5938643da3145d523e5d53b46b034e9ed2
angel-prz <191927151+angel-prz@users.noreply.github.com>
2025-07-28 23:43:15 -0300

    build: cors para recever request do frontend

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -15,6 +15,7 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
+        $middleware->append(\Illuminate\Http\Middleware\HandleCors::class);
         $middleware->api(append:ForceJsonResponse::class);
         /*$middleware->statefulApi(); */
         $middleware->alias([
