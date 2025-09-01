dd672a0afa68da6d8656413e186bf79c0dd13cd9
Angel Pérez <191927151+angel-prz@users.noreply.github.com>
2025-09-01 16:44:04 +0000

    feat: cria classe SaudeMedica

---
+++ app/Http/Controllers/SaudeMedicaController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreSaudeMedicaRequest;
+use App\Http\Requests\UpdateSaudeMedicaRequest;
+use App\Models\SaudeMedica;
+
+class SaudeMedicaController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreSaudeMedicaRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(SaudeMedica $saudeMedica)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(SaudeMedica $saudeMedica)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateSaudeMedicaRequest $request, SaudeMedica $saudeMedica)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(SaudeMedica $saudeMedica)
+    {
+        //
+    }
+}
