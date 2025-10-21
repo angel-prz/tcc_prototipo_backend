886f1aa9e00ac6469eace9ade2e7e7b5b2fd389d
Angel Pérez <191927151+angel-prz@users.noreply.github.com>
2025-10-21 17:03:14 +0000

    refactor: (frontend: consultas) add metodos createConsulta e editConsulta

--- resources/frontend/src/contexts/ConsultaProvider.jsx
+++ resources/frontend/src/contexts/ConsultaProvider.jsx
@@ -14,22 +14,25 @@ export const ConsultasContext = createContext({
 });
 
 const ConsultaProvider = ({ children }) => {
-    const [data, setData] = useState(null);
+    const [data, setData] = useState([]);
     const [isLoaded, setIsLoaded] = useState(false);
 
     const loadConsultas = async (id = null) => {
+        setIsLoaded(false);
         const url = id ? `/consultas/${id}` : `/consultas`;
         try {
             const { data } = await axiosClient.get(url);
             const _data = data?.data;
             console.log({ _data });
 
-            if (!_data) throw new Error("Erro ao carregar usuarios");
+            if (!_data) throw new Error("Erro ao carregar consultas");
 
             Array.isArray(_data) && _data.reverse();
             setData(_data);
+            setIsLoaded(true);
         } catch (error) {
             console.log(error);
+            setIsLoaded(true);
         }
     };
 
@@ -52,8 +55,27 @@ const ConsultaProvider = ({ children }) => {
         }
     };
 
-    const editConsulta = () => {
-        return true; //TODO
+    const editConsulta = async (id, consulta = null) => {
+        /* return true; //TODO */
+        try {
+            console.log(`Atualizar consulta id: ${id}`, { consulta });
+            const { data } = await axiosClient.put(
+                `/consultas/${id}`,
+                consulta
+            );
+            if (!data) throw new Error("Error ao atualizar consulta");
+            const _data = data?.data;
+            const { message } = data;
+
+            console.log({ _data, message });
+            loadConsultas();
+            return message;
+        } catch (error) {
+            console.error(error);
+            return (
+                error?.response?.data?.message || "Erro ao atualizar a consulta"
+            );
+        }
     };
 
     const deleteConsulta = async (consulta = null) => {
@@ -61,11 +83,25 @@ const ConsultaProvider = ({ children }) => {
         const data = await axiosClient.delete(`/consultas/${consulta.id}`);
         console.log({ data });
         loadConsultas();
-        return message;
+        return true;
     };
 
-    const createConsulta = () => {
-        return true; //TODO
+    const createConsulta = async (consulta = null) => {
+        try {
+            if (!consulta) throw Error("Consulta inválida");
+            console.log(`Criar nova consulta`, { consulta });
+            const data = axiosClient.post("/consultas", consulta);
+            if (!data) throw new Error("Erro ao criar consulta");
+            const _data = data?.data;
+            const { message } = data;
+
+            console.log({ data }, message);
+            loadConsultas();
+            return message;
+        } catch (error) {
+            console.error(error);
+            return error?.response?.data?.message || "Erro ao criar a cosnulta";
+        }
     };
 
     useEffect(() => {
